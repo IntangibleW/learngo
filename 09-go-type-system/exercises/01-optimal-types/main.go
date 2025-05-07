1b19f0de32c85014907892ec77e40d6120a5a6bc
IntangibleW <IntangibleW@users.noreply.github.com>
2025-05-07 23:28:20 +0800

    modified:   09-go-type-system/exercises/01-optimal-types/main.go 	modified:   09-go-type-system/exercises/02-the-type-problem/main.go 	modified:   09-go-type-system/exercises/03-parse-arg-numbers/main.go

--- 09-go-type-system/exercises/01-optimal-types/main.go
+++ 09-go-type-system/exercises/01-optimal-types/main.go
@@ -8,6 +8,11 @@
 
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Optimal Types
 //
@@ -36,16 +41,30 @@ func main() {
 	// ---
 
 	// an english letter (search web for: ascii control code)
+	var letter byte = 'A'
+	fmt.Println("An english letter: ", letter)
 
 	// a non-english letter (search web for: unicode codepoint)
+	var unicode rune = 'Ç'
+	fmt.Println("A non-english letter: ", unicode)
 
 	// a year in 4 digits like 2040
+	var year uint16 = 2040
+	fmt.Println("A year in 4 digits like 2040: ", year)
 
 	// a month in 2 digits: 1 to 12
+	var month uint8 = 6
+	fmt.Println("A month in 2 digits: 1 to 12: ", month)
 
 	// the speed of the light
+	var lightSpeed uint32 = 299792458 // meters
+	fmt.Println("The speed of light: ", lightSpeed)
 
 	// angle of a circle
+	var angle float32 = 35.8
+	fmt.Println("Angle of a circle: ", angle)
 
 	// PI number: 3.141592653589793
+	var pi float64 = math.Pi
+	fmt.Println("PI number: ", pi)
 }
