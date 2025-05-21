f817f27856977619665b07bca45d3087a441ba80
IntangibleW <43871076+IntangibleW@users.noreply.github.com>
2025-05-21 12:34:01 +0800

    modified:   ../01-printing-the-digits/main.go

--- 15-project-retro-led-clock/01-printing-the-digits/main.go
+++ 15-project-retro-led-clock/01-printing-the-digits/main.go
@@ -8,5 +8,100 @@
 
 package main
 
+import "fmt"
+
 func main() {
+	type placeholder [5]string
+
+	var (
+		zero = placeholder{
+			"███",
+			"█ █",
+			"█ █",
+			"█ █",
+			"███",
+		}
+		one = placeholder{
+			"██ ",
+			" █ ",
+			" █ ",
+			" █ ",
+			"███",
+		}
+
+		two = placeholder{
+			"███",
+			"  █",
+			"███",
+			"█  ",
+			"███",
+		}
+
+		three = placeholder{
+			"███",
+			"  █",
+			"███",
+			"  █",
+			"███",
+		}
+
+		four = placeholder{
+			"█ █",
+			"█ █",
+			"███",
+			"  █",
+			"  █",
+		}
+
+		five = placeholder{
+			"███",
+			"█  ",
+			"███",
+			"  █",
+			"███",
+		}
+
+		six = placeholder{
+			"███",
+			"█  ",
+			"███",
+			"█ █",
+			"███",
+		}
+
+		seven = placeholder{
+			"███",
+			"  █",
+			"  █",
+			"  █",
+			"  █",
+		}
+
+		eight = placeholder{
+			"███",
+			"█ █",
+			"███",
+			"█ █",
+			"███",
+		}
+
+		nine = placeholder{
+			"███",
+			"█ █",
+			"███",
+			"  █",
+			"███",
+		}
+	)
+
+	digits := [...]placeholder{
+		zero, one, two, three, four, five, six, seven, eight, nine,
+	}
+
+	for line := range digits[0] {
+		for digit := range digits {
+			fmt.Print(digits[digit][line], " ")
+		}
+		fmt.Println()
+	}
 }
